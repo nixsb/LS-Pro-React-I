b59cf72811030b20d90e80d97ae2022938bf648c
Nikki Byrne <nikki.byrne@wlt.com>
2017-03-22 17:01:23 +0000

    navbar and body added to react project

---
+++ profile/src/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Body.css';
+
+export default class Body extends Component {
+  render() {
+    return (
+      <div className="main">
+        <p>body copy</p>
+      </div>
+    );
+  }
+}
+
+
---
+++ profile/src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <h1>title of your page</h1>
+      </div>
+    );
+  }
+}
+
+
